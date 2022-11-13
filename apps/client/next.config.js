340a34742d1898222e52188ec057127690b8ba7d
FreeAoi <notdevelopergaming@gmail.com>
2022-11-12 19:04:44 -0600

    Initial files

---
+++ apps/client/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	compress: true,
+};
+
+module.exports = nextConfig;
---
+++ apps/server/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+    imports: [],
+    controllers: [],
+    providers: []
+})
+class AppModule {}
+
+export default AppModule;
